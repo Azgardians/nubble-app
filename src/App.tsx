35ce9a45c798bdf502db578bea30e27b2697ae41
azgardians <b517007@iiit-bh.ac.in>
2023-08-14 23:29:28 +0530

    Added the number pad

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,17 @@ function TopBlock ({letterName}: {letterName: string}) {
   )
 }
 
-// function Block () {
-//   return (
-
-//   )
-// }
+function Block ({valueArr, index}: {valueArr: number[], index: number}) {
+  return (
+    <div className='d-flex m-0'>
+      {valueArr.map((item: number, j: number) => (
+        <div className='boxBlock d-flex align-items-center justify-content-center' key={'' + item + index + j}>
+          <span className='boxValue'>{item === 0 ? '' : item}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 // This is the board structure
 function Board () {
@@ -25,12 +31,16 @@ function Board () {
         <div className="level-1">
           <div className="container1">
             {letterValues.map((item:string, index: number) => (
-              <TopBlock letterName={item.toUpperCase()} key={item+index} />
+              <TopBlock letterName={item.toUpperCase()} key={'' + item+index} />
             ))}
           </div>
         </div>
-        <div className="level-2 pt-4">
-            
+        <div className="level-2 pt-5 d-flex justify-content-center">
+          <div className='mt-5 me-5 pe-3'>
+            {values.map(((iarr: number[], i: number) => (
+              <Block valueArr={iarr} index={i} key={i} />
+            )))}
+          </div>
         </div>
       </div>
     </div>
